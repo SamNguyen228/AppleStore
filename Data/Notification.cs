59254b4fe554ac5d9e8cce13d97a92375c06800a
SamNguyen228 <duonghoangsamet@hoangsametgmail.sametgmail.com>
2025-04-09 14:23:04 +0700

    Thêm .gitignore và dọn project

---
+++ Data/Notification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebsiteTMDT.Data;
+
+public class Notification
+{
+    public int NotificationId { get; set; }
+    public string Content { get; set; }
+    public bool IsRead { get; set; } = false;
+    public DateTime CreatedAt { get; set; }
+}
+
